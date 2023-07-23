d56f26bca486f63db4a4d7b57d2dd6f94ebcdc50
japhethbear <japhethbear@gmail.com>
2023-07-22 22:44:54 -0500

    first commit

---
+++ algos.js
@@ -0,0 +1,15 @@
+// Algorithms in JavaScript
+
+
+// 1. Bubble Sort
+
+
+
+// 2. Selection Sort
+
+
+
+// 3. Insertion Sort
+
+
+
---
+++ algos.py
@@ -0,0 +1,12 @@
+# Algoriths in Python 
+
+# 1. Binary Search
+
+
+
+# 2. Bubble Sort
+
+
+# 3. Insertion Sort
+
+
