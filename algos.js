0fa2d4281dd998e5a2d8551f70178920eaf7e256
japhethbear <japhethbear@gmail.com>
2023-07-22 22:53:46 -0500

    First Algos

--- algos.js
+++ algos.js
@@ -1,15 +1,34 @@
 // Algorithms in JavaScript
 
+// Algorithm Prompt 1: Sum of Two Numbers
 
-// 1. Bubble Sort
+// Problem: Write a function called sumOfTwo that takes two numbers as input and returns their sum.
 
+// Input: Two numbers, num1 and num2.
 
+// Output: The sum of num1 and num2.
 
-// 2. Selection Sort
+// Example: console.log(sumOfTwo(5, 7)); // Output: 12
+// console.log(sumOfTwo(-3, 10)); // Output: 7
+// console.log(sumOfTwo(0, 0)); // Output: 0
 
+// / My solution:
 
+function sumOfTwo(num1, num2) {
+    return num1 + num2;
+}
 
-// 3. Insertion Sort
+// Algorithm Prompt 2: Find the Maximum Number
 
+// Problem: Write a function called findMax that takes an array of numbers as input and returns the largest number in the array.
 
+// Input: An array of numbers, arr.
+
+// Output: The largest number in the array.
+
+// Example: console.log(findMax([5, 2, 9, 1, 5, 6])); // Output: 9
+// console.log(findMax([-3, 0, -10, -1])); // Output: 0
+// console.log(findMax([8])); // Output: 8
+
+// My solution:
 
